884b5ea3aa8ac9f33b2d690ba3fca30660040ef4
liuyuqi789 <liuyuqi767@163.com>
2020-01-09 19:08:28 +0800

    20-1-9

---
+++ 正式课第十一周/day3/app/src(登录验证)/views/login.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+        <p>LOGIN</p>
+        用户名:<input type="text"  v-model="val">
+        <button @click="click">点击登录</button>
+    </div>
+</template>
+<script>
+//https://www.cnblogs.com/xuxinstyle/p/9675541.html  token知识点
+import {get,post} from '../api/api';
+    export default {
+        data(){
+            return {
+                val:''
+            }
+        },
+        methods:{
+            click(){
+                post(this.val).then(data=>{
+                    if(data.code === 0){
+                        this.$router.push('/');
+                    }else{
+                        alert('登录失败');
+                    }
+                    this.val = '';
+                })
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
